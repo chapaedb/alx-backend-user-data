f51be10f6d0fb24aba6164775db246f13e4a2a46
chapaedb <chapaeresso1@gmail.com>
2024-07-11 01:19:54 +0300

    additional module import done

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3
+"""Authentication module.
+"""
 from flask import request
+from typing import List, TypeVar
+import fnmatch
 
 class Auth:
+
     def require_auth(self, path: str,excluded_paths: List[str]) -> bool:
         return False
     def authorization_header(self, request=None) -> str:
