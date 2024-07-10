918889fe380638d992cfd58ac85bdf6045ce8cd8
chapaedb <chapaeresso1@gmail.com>
2024-07-11 01:18:13 +0300

    Update

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -1,6 +1,6 @@
 from flask import request
 
-class Auth():
+class Auth:
     def require_auth(self, path: str,excluded_paths: List[str]) -> bool:
         return False
     def authorization_header(self, request=None) -> str:
