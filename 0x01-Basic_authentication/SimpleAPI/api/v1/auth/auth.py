7e5262506e02bfd3e8e6b2ee3a32ae032104677a
chapaedb <chapaeresso1@gmail.com>
2024-07-11 01:13:59 +0300

    Added the auth blueprint

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,9 @@
+from flask import request
+
+class Auth():
+    def require_auth(self, path: str,excluded_paths: List[str]) -> bool:
+        return False
+    def authorization_header(self, request=None) -> str:
+        return None
+    def current_user(self, request=None) -> TypeVar('User'): 
+        return None
