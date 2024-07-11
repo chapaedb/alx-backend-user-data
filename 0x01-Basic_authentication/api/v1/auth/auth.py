5c95a6bae301a31a9a32fc3ff86bf5617db1a954
chapaedb <chapaeresso1@gmail.com>
2024-07-11 10:23:00 +0300

    Update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,12 +6,21 @@ from typing import List, TypeVar
 import fnmatch
 
 class Auth:
-
+    """
+    Authorization required functionalities require this
+    """
     def require_auth(self, path: str,excluded_paths: List[str]) -> bool:
+        """Returns True if the path is not in the list of strings excluded_paths"""
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
+        if path[-1] != '/':
+            path = path + '/'
+
         for excluded in excluded_paths:
-            if path == excluded.rstrip('/'):
+            if excluded[-1] != '/':
+                excluded = excluded + '/'
+
+            if path == excluded:
                 return False
         return True
     def authorization_header(self, request=None) -> str:
