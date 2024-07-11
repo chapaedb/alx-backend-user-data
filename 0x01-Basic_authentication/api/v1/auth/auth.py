16cc18e927566736c1f96ce745292f3ae89edfc2
chapaedb <chapaeresso1@gmail.com>
2024-07-11 10:05:43 +0300

    Updated the require_oath method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,12 @@ import fnmatch
 class Auth:
 
     def require_auth(self, path: str,excluded_paths: List[str]) -> bool:
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excluded in excluded_paths:
+            if path == excluded.rstrip('/'):
+                return False
+        return True
     def authorization_header(self, request=None) -> str:
         return None
     def current_user(self, request=None) -> TypeVar('User'): 
